3e40703a99fd64dbb805f144e4f862e3b9dfa63f
Michał B <cyberwolf_96@outlook.com>
2018-08-16 00:17:32 +0200

    Working on SQLite database and UWP project.

---
+++ Deskberry/Deskberry.SQLite/Data/DeskberryContext.cs
@@ -0,0 +1,25 @@
+﻿using Deskberry.SQLite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deskberry.SQLite.Data
+{
+    public class DeskberryContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public DeskberryContext(DbContextOptions<DeskberryContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=deskberry.db");
+        }
+    }
+}
---
+++ Deskberry/Deskberry.SQLite/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deskberry.SQLite.Models
+{
+    public class Account
+    {
+        public int Id { get; protected set; }
+        public string Login { get; protected set; }
+        public byte[] PasswordHash { get; protected set; }
+        public byte[] Salt { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
