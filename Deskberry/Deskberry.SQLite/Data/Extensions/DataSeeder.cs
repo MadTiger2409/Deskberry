8993bdaba12d1a7cf0315ee782aff6be2f90cb25
Michał B <cyberwolf_96@outlook.com>
2018-08-26 01:16:13 +0200

    Working on IoC implementation and default data initialization.

---
+++ Deskberry/Deskberry.SQLite/Data/Extensions/DataSeeder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deskberry.SQLite.Data.Extensions
+{
+    public static class DataSeeder
+    {
+        public static void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
