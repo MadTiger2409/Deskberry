02228d1975340a460c12ebec49e45e423585f168
Michał B <MadTiger2409@users.noreply.github.com>
2019-04-23 14:36:17 +0200

    Working on SuperNotes visual.

---
+++ Deskberry/Deskberry.UWP/ViewModels/Notes/AddNotePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deskberry.UWP.ViewModels.Notes
+{
+    public class AddNotePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
