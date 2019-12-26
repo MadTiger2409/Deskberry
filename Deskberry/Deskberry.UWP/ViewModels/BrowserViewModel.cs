1fa1ca00b9437533f2d863e907ce20860a8d8504
Michał B <cyberwolf_96@outlook.com>
2019-12-26 16:04:42 +0100

    Working on Browser

--- Deskberry/Deskberry.UWP/ViewModels/BrowserViewModel.cs
+++ Deskberry/Deskberry.UWP/ViewModels/BrowserViewModel.cs
@@ -1,9 +1,31 @@
-﻿using System.ComponentModel;
+﻿using Deskberry.UWP.Commands;
+using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
 
 namespace Deskberry.UWP.ViewModels
 {
     public class BrowserViewModel : INotifyPropertyChanged
     {
+        public BrowserViewModel()
+        {
+            WebView = new WebView();
+
+            GoBackwardCommand = new RelayCommand(() => WebView.GoBack(), CanGoBackward);
+            GoForwardCommand = new RelayCommand(() => WebView.GoForward(), CanGoForward);
+            RefreshCommand = new RelayCommand(() => WebView.Refresh());
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public RelayCommand GoBackwardCommand { get; private set; }
+        public RelayCommand GoForwardCommand { get; private set; }
+        public RelayCommand GoHomeCommand { get; private set; }
+        public RelayCommand RefreshCommand { get; private set; }
+
+        public WebView WebView { get; set; }
+
+        private bool CanGoBackward() => WebView.CanGoBack;
+
+        private bool CanGoForward() => WebView.CanGoForward;
     }
 }
