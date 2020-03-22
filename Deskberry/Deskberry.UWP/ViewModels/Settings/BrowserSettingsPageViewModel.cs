9d514525eb1c9ec6673b46b32fa2d50bdebf610d
Michał B <MadTiger2409@users.noreply.github.com>
2020-03-22 15:09:42 +0100

    Working

--- Deskberry/Deskberry.UWP/ViewModels/Settings/BrowserSettingsPageViewModel.cs
+++ Deskberry/Deskberry.UWP/ViewModels/Settings/BrowserSettingsPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Deskberry.Tools.Services.Interfaces;
+using Deskberry.UWP.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace Deskberry.UWP.ViewModels.Settings
 {
     public class BrowserSettingsPageViewModel
     {
+        private readonly IFavoriteService _favoriteService;
+        private readonly IHomePageService _homePageService;
+
+        public BrowserSettingsPageViewModel(IHomePageService homePageService, IFavoriteService favoriteService)
+        {
+            _homePageService = homePageService;
+            _favoriteService = favoriteService;
+        }
+
+        public RelayCommand DeleteCommand { get; set; }
+        public RelayCommand SaveCommand { get; set; }
+
+        public async Task InitializeDataAsync()
+        {
+            //TODO load home page there
+        }
     }
 }
