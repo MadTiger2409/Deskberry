05739afc08decf7ea5c0ef279a24ed40922bce89
Michał B <MadTiger2409@users.noreply.github.com>
2020-06-03 00:13:40 +0200

    working on CreateAccount command class and validation

--- Deskberry/Deskberry.CommandValidation/CommandObjects/Account/CreateAccount.cs
+++ Deskberry/Deskberry.CommandValidation/CommandObjects/Account/CreateAccount.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Deskberry.CommandValidation.CommandObjects.Account
 {
-    public class CreateAccount
+    public class CreateAccount : INotifyPropertyChanged
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
+        private string _login;
+        private string _password;
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                if (value == _login)
+                    return;
+
+                _login = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Login)));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (value == _password)
+                    return;
+
+                _password = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Password)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
