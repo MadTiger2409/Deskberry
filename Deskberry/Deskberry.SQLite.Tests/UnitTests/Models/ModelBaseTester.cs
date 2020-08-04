cc61e081fadb06c1d77010fcd89813557fac8c57
Michał B <MadTiger2409@users.noreply.github.com>
2020-08-04 13:52:21 +0200

    Working at unit tests

---
+++ Deskberry/Deskberry.SQLite.Tests/UnitTests/Models/ModelBaseTester.cs
@@ -0,0 +1,27 @@
+﻿using Deskberry.Common.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Deskberry.SQLite.Tests.UnitTests.Models
+{
+    [Collection("Model Base Tests")]
+    public class ModelBaseTester
+    {
+        [Fact]
+        public void ModelBase_CreateObject_Success()
+        {
+            // Arrange
+            ModelBase model;
+            var startDate = DateTime.UtcNow;
+
+            // Act
+            model = new ModelBase();
+
+            // Assert
+            Assert.NotEqual(default, model.CreatedAt);
+            Assert.True(model.CreatedAt.Ticks > startDate.Ticks);
+        }
+    }
+}
