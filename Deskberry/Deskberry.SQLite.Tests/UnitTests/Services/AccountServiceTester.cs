014030030a1aacbeea8ede541d5b9edd554565dd
Michał B <MadTiger2409@users.noreply.github.com>
2020-08-17 19:47:17 +0200

    Working on service tests

--- Deskberry/Deskberry.SQLite.Tests/UnitTests/Services/AccountServiceTester.cs
+++ Deskberry/Deskberry.SQLite.Tests/UnitTests/Services/AccountServiceTester.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Deskberry.CommandValidation.CommandObjects.Account;
+using Deskberry.Services;
+using Deskberry.Tests.Resources.Databases;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Deskberry.Tests.UnitTests.Services
 {
     [Collection("Account service's tests")]
-    public class AccountServiceTester
+    public class AccountServiceTester : SQLiteDatabaseFixture
     {
+        [Fact]
+        public async Task AccountService_AddAccount_ArgumentNullException()
+        {
+            // Arrange
+            CreateAccount createAccountCommand = null;
+            AccountService accountService = new AccountService(DbContext);
+
+            // Act and assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => accountService.AddAccountAsync(createAccountCommand));
+        }
     }
 }
